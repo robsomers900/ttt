c2d4399422920ef36d65d05dcb210da3f907384c
Rob Somers <64647442+robsomers900@users.noreply.github.com>
2024-09-19 14:34:55 +0100

    Add loop to create game that accepts prompt - Need to address the issue of the game not ending when 3 i a row happens

--- app.js
+++ app.js
@@ -8,12 +8,16 @@ function createGameBoard () {
 }
 
 //winner function - refer to notes
-function createControl(board){
+function createControl(){
+    const playerX = createPlayer("playerX", "X")
+    const playerO = createPlayer("playerO", "O")
+    let board = createGameBoard()
     const move = (player, position) => {
-           symbol = player.playerSymbol
+           let symbol = player.playerSymbol
            board.board[position[0]][position[1]] = symbol
+           console.log(JSON.stringify(board.board))
     }
-    const checkWinner = (playerX, playerO) => {
+    const checkWinner = () => {
         //check each consecutive 3 for all X or all O and return 1 or 0 and playerID
         let gameStatus = 0;
         let winner = null;
@@ -41,7 +45,7 @@ function createControl(board){
                     }
             else if (board.board[2][0] === "O" &&
                 board.board[1][1] === "O" &&
-                board.board[2][0] === "O"){
+                board.board[0][2] === "O"){
                     gameStatus = 1; //gameover
                     winner = playerO
                     }
@@ -62,17 +66,37 @@ function createControl(board){
         }
         return {gameStatus, winner}
     }
-    return {board, move, checkWinner}
+    return {board, move, checkWinner, playerX, playerO}
 }
 
-const playerX = createPlayer("playerX", "X")
-const playerO = createPlayer("playerO", "O")
-const gameBoard = createGameBoard()
 
 
-control = createControl(gameBoard)
-control.move(playerX, [0, 0])
-control.move(playerX, [1,1])
-control.move(playerX, [2,2])
-console.log(gameBoard.board)
-console.log(control.checkWinner(playerX, playerO))
+
+control = createControl()
+
+player = 0 //playerO
+while(control.checkWinner().gameStatus === 0){
+    if (player === 0){
+        let moveParams = prompt("O Player, enter row, column:")
+        moveParams = moveParams.split(",").map(Number)
+        control.move(control.playerO, moveParams)
+        player = 1
+        console.log(control.checkWinner(control.playerX, control.playerO))
+    }
+    else if (player === 1){
+        let moveParams = prompt("X Player, enter row, column:")
+        moveParams = moveParams.split(",").map(Number)
+        control.move(control.playerX, moveParams)
+        player = 0
+        console.log(control.checkWinner(control.playerX, control.playerO))
+    }
+
+}
+
+
+
+// control.move(control.playerX, [0, 0])
+// console.log(control.checkWinner(control.playerX, control.playerO))
+// control.move(control.playerX, [1, 1])
+// console.log(control.checkWinner(control.playerX, control.playerO))
+
