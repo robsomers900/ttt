e67ffd3a333be9834a45273667c19c668fad5f1f
Rob Somers <64647442+robsomers900@users.noreply.github.com>
2024-09-20 14:38:08 +0100

    Complete functionality

--- app.js
+++ app.js
@@ -17,6 +17,29 @@ function createControl(){
            board.board[position[0]][position[1]] = symbol
            console.log(JSON.stringify(board.board))
     }
+
+    //add condition to all winning statements to pop up modal - player wins, play again button that reloads page
+modal = document.createElement("dialog")
+modal.classList.add("modal")
+mainContainer = document.querySelector(".main-container")
+mainContainer.appendChild(modal)
+modalContainer = document.createElement("div")
+modalContainer.classList.add("modalContainer")
+modalMessage = document.createElement("span")
+modalMessage.classList.add("modalMessage")
+modal.appendChild(modalContainer)
+modalContainer.appendChild(modalMessage)
+newGameButton = document.createElement("button")
+newGameButton.classList.add("button-18")
+newGameButton.innerHTML = "Start New Game"
+modalContainer.appendChild(newGameButton)
+
+// this can be updated
+newGameButton.addEventListener("click", () => {
+    location.reload(true)
+})
+
+
     const checkWinner = () => {
         //check each consecutive 3 for all X or all O and return 1 or 0 and playerID
         let gameStatus = 0;
@@ -28,24 +51,32 @@ function createControl(){
                 board.board[i][2] === "O"){
                     gameStatus = 1; //gameover
                     winner = playerO
+                    modalMessage.innerHTML = "X Wins!"
+                    modal.showModal()
             }
             else if (board.board[i][0] === "X" &&
                     board.board[i][1] === "X" &&
                     board.board[i][2] === "X"){
                         gameStatus = 1; //gameover
                         winner = playerX
+                        modalMessage.innerHTML = "X Wins!"
+                        modal.showModal()
             }
             if (board.board[0][i] === "O" &&
                 board.board[1][i] === "O" &&
                 board.board[2][i] === "O"){
                     gameStatus = 1; //gameover
                     winner = playerO
+                    modalMessage.innerHTML = "O Wins!"
+                    modal.showModal()
             }
             else if (board.board[0][i] === "X" &&
                     board.board[1][i] === "X" &&
                     board.board[2][i] === "X"){
                         gameStatus = 1; //gameover
                         winner = playerX
+                        modalMessage.innerHTML = "X Wins!"
+                        modal.showModal()
             }
         }
         if (gameStatus === 0){
@@ -54,24 +85,32 @@ function createControl(){
                 board.board[2][2] === "O"){
                     gameStatus = 1; //gameover
                     winner = playerO
+                    modalMessage.innerHTML = "O Wins!"
+                    modal.showModal()
                     }
             else if (board.board[2][0] === "O" &&
                 board.board[1][1] === "O" &&
                 board.board[0][2] === "O"){
                     gameStatus = 1; //gameover
                     winner = playerO
+                    modalMessage.innerHTML = "O Wins!"
+                    modal.showModal()
                     }
             else if(board.board[0][0] === "X" &&
                 board.board[1][1] === "X" &&
                 board.board[2][2] === "X"){
                     gameStatus = 1; //gameover
                     winner = playerX
+                    modalMessage.innerHTML = "X Wins!"
+                    modal.showModal()
                     }
             else if (board.board[2][0] === "X" &&
                 board.board[1][1] === "X" &&
                 board.board[2][0] === "X"){
                     gameStatus = 1; //gameover
                     winner = playerX
+                    modalMessage.innerHTML = "X Wins!"
+                    modal.showModal()
                     }
                     
 
@@ -91,7 +130,6 @@ control = createControl()
 const squares = Array.from(document.querySelectorAll(".square"))
 console.log(squares)
 
-//add condition for if gamestatus is 1 to pop up modal
 squares.forEach((square, index) => {
     square.addEventListener("click", () => {
     if (player === 0){
