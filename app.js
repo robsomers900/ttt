9beb1d20bd3e43545da26e56ad28d46c67af66cc
Rob Somers <64647442+robsomers900@users.noreply.github.com>
2024-09-20 13:40:45 +0100

    Implement near full functionality with console showing result

--- app.js
+++ app.js
@@ -24,16 +24,28 @@ function createControl(){
         //check each row and column combo
         for (i = 0; i <3; i++){
             if (board.board[i][0] === "O" &&
-                board.board[i][0] === board.board[i][1] &&
-                board.board[i][0] === board.board[i][2]){
+                board.board[i][1] === "O" &&
+                board.board[i][2] === "O"){
                     gameStatus = 1; //gameover
                     winner = playerO
             }
             else if (board.board[i][0] === "X" &&
-                board.board[i][0] === board.board[i][1] &&
-                board.board[i][0] === board.board[i][2]){
+                    board.board[i][1] === "X" &&
+                    board.board[i][2] === "X"){
+                        gameStatus = 1; //gameover
+                        winner = playerX
+            }
+            if (board.board[0][i] === "O" &&
+                board.board[1][i] === "O" &&
+                board.board[2][i] === "O"){
                     gameStatus = 1; //gameover
-                    winner = playerX
+                    winner = playerO
+            }
+            else if (board.board[0][i] === "X" &&
+                    board.board[1][i] === "X" &&
+                    board.board[2][i] === "X"){
+                        gameStatus = 1; //gameover
+                        winner = playerX
             }
         }
         if (gameStatus === 0){
@@ -70,33 +82,58 @@ function createControl(){
 }
 
 
+player = 0
 
+indexConverterArray = [[0,0], [0,1], [0,2], [1,0], [1,1], [1,2], [2,0], [2,1], [2,2]]
 
 control = createControl()
 
-player = 0 //playerO
-while(control.checkWinner().gameStatus === 0){
+const squares = Array.from(document.querySelectorAll(".square"))
+console.log(squares)
+
+//add condition for if gamestatus is 1 to pop up modal
+squares.forEach((square, index) => {
+    square.addEventListener("click", () => {
     if (player === 0){
-        let moveParams = prompt("O Player, enter row, column:")
-        moveParams = moveParams.split(",").map(Number)
+        let moveParams = indexConverterArray[index]
         control.move(control.playerO, moveParams)
         player = 1
         console.log(control.checkWinner(control.playerX, control.playerO))
+        square.innerHTML = "O"
     }
     else if (player === 1){
-        let moveParams = prompt("X Player, enter row, column:")
-        moveParams = moveParams.split(",").map(Number)
+        let moveParams = indexConverterArray[index]
         control.move(control.playerX, moveParams)
         player = 0
         console.log(control.checkWinner(control.playerX, control.playerO))
+        square.innerHTML = "X"
     }
+        
+    })
+})
 
-}
 
 
 
-// control.move(control.playerX, [0, 0])
-// console.log(control.checkWinner(control.playerX, control.playerO))
-// control.move(control.playerX, [1, 1])
-// console.log(control.checkWinner(control.playerX, control.playerO))
+
+// player = 0 //playerO
+// while(control.checkWinner().gameStatus === 0){
+//     if (player === 0){
+//         let moveParams = prompt("O Player, enter row, column:")
+//         moveParams = moveParams.split(",").map(Number)
+//         control.move(control.playerO, moveParams)
+//         player = 1
+//         console.log(control.checkWinner(control.playerX, control.playerO))
+//     }
+//     else if (player === 1){
+//         let moveParams = prompt("X Player, enter row, column:")
+//         moveParams = moveParams.split(",").map(Number)
+//         control.move(control.playerX, moveParams)
+//         player = 0
+//         console.log(control.checkWinner(control.playerX, control.playerO))
+//     }
+
+// }
+
+
 
